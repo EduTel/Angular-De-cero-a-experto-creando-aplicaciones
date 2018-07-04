20732b0f3ef576982c636c5413f5df506611c0c4
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-07-04 16:05:29 -0500

    13. Introducción a los tipos de datos

---
+++ tipo de dato/index.ts
@@ -0,0 +1,26 @@
+(function var_var(){
+  let nombre = "hola";
+  nombre=1;
+
+  let nombre2:String;
+  nombre2 = "hola";
+  nombre2 = 1;
+  nombre2 = 2;
+
+  let Number: Number;
+  let Boolean: Boolean;
+  /*tipos compuestos*/
+  let date:Date = new Date();
+  console.log(date);
+  date = new Date("2018-07-03");
+  console.log(date);
+
+  let spiderman = {
+    nombre: "peter",
+    edad: 20
+  }
+  spiderman = {
+    nombre: "peter",
+    edad:21
+  }
+})();
