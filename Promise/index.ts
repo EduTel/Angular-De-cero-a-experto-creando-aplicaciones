3fa491be91aff05a198ac779855fee5b67963181
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-07-09 14:36:34 -0500

    Promise

---
+++ Promise/index.ts
@@ -0,0 +1,15 @@
+(function ejecutar(){
+  let prom1 = new Promise(function(resolve:any,reject:any){
+    setTimeout(() => {
+      resolve();
+      reject();
+    }, 1500);
+  });
+  console.log("paso 1");
+  prom1.then(()=>{
+    console.log("Ejecutarme cuando se termine bien");
+  },()=>{
+    console.error("Ejecutarme cuando algo salga mal");
+  });
+  console.log("paso 2");
+})();
