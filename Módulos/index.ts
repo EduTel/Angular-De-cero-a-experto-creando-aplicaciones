accc4751bb7db447d4c05ddd3e1d46b8990c63bf
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-07-10 09:42:37 -0500

    import y export

---
+++ Módulos/index.ts
@@ -0,0 +1,7 @@
+import { Xmen, Villano } from "./exports.class";
+
+let wolverine:Xmen = new Xmen("IRONMAN","Tony");
+wolverine.imprimir();
+
+let tanos: Villano = new Villano("Tanos", "destruir media galaxia");
+tanos.imprimir();
---
+++ Módulos/villano.clases.ts
@@ -0,0 +1,11 @@
+export class Villano{
+  nombre:string;
+  plan:string;
+  constructor(nombre:string,plan:string){
+    this.nombre=nombre;
+    this.plan=plan;
+  }
+  imprimir(){
+    console.log(`El plan es: ${this.plan}`);
+  }
+}
