7210f9d207ec19573a772984431468cd86f63ffd
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-07-10 10:18:26 -0500

    @decorator

---
+++ Decoradores/index.ts
@@ -0,0 +1,11 @@
+(function ejecutar() {
+  function consola(constructor:Function){
+    console.log(constructor);
+  }
+  @consola
+  class Villano{
+    constructor(public nombre:string){
+
+    }
+  }
+})();
