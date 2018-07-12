1da1ba4cfb2e0469b1c40e1a92d30b90b46a4dc2
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-07-12 16:59:52 -0500

    routerLink, routerLinkActive y animated fadeIn fast

--- 02SinglePageAplication/src/app/app.routes.ts
+++ 02SinglePageAplication/src/app/app.routes.ts
@@ -3,23 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 
 // componentes
 import { HomeComponent } from './shared/home/home.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { AboutComponent } from './shared/about/about.component';
 import { WriterComponent } from './shared/writer/writer.component';
+import { AboutComponent } from './shared/about/about.component';
 
 
 
 const ROUTES: Routes = [
-  { path: 'home', component: HomeComponent   },
+  { path: 'home', component: HomeComponent, data: { title: ',me home' }},
+  { path: 'writer', component: WriterComponent, data: { title: ',me writer' } },
+  { path: 'about', component: AboutComponent, data: { title: ',me about' } },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 @NgModule({
   // imports: [RouterModule.forRoot(ROUTES, {useHash: true})],
-  imports: [RouterModule.forRoot(ROUTES)],
+  imports: [RouterModule.forRoot(ROUTES, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
+
 
 
 /*
