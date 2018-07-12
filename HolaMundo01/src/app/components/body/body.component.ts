ac70cbfd7a0ea7c3969916ebcf490a1a58e0081d
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-07-12 10:17:22 -0500

    *ngIf Y *ngFor

--- HolaMundo01/src/app/components/body/body.component.ts
+++ HolaMundo01/src/app/components/body/body.component.ts
@@ -4,4 +4,10 @@ import { Component } from '@angular/core';
   templateUrl: './body.component.html'
 })
 export class BodyComponent {
+  mostrar = true;
+  frase: { mensaje: string, autor: string } = {
+    autor: `Albert camus`,
+    mensaje: 'No hay alegria de vivir, sin desesperacion de vivir'
+  };
+  personajes: string[] = ['Albert camus', 'Hermann Hesse', 'Carlos fuentes'];
 }
