5563392b9c2b360cdc84bc45b46174319351072b
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-07-16 11:13:20 -0500

    Buscando

--- 02SinglePageAplication/src/app/shared/show-search-writer/show-search-writer.component.ts
+++ 02SinglePageAplication/src/app/shared/show-search-writer/show-search-writer.component.ts
@@ -2,17 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { WriterService, Writer } from '../../servicios/writes.service';
+import { WritersComponent } from '../writers/writers.component';
 
 @Component({
   selector: 'app-show-search-writer',
   templateUrl: './show-search-writer.component.html',
   // styleUrls: ['./show-search-writer.component.css']
 })
-export class ShowSearchWriterComponent implements OnInit {
+export class ShowSearchWriterComponent implements OnInit  {
   private writes: Writer[];
+  private termino: string;
   constructor(private _ActivatedRoute: ActivatedRoute, private _write_service: WriterService) {
     this._ActivatedRoute.params.subscribe(parametros => {
       console.log(parametros['termino']);
+      this.termino = parametros['termino'];
       this.writes = this._write_service.buscar(parametros['termino']);
       console.log('_______________________________________\n');
       console.log(this.writes);
