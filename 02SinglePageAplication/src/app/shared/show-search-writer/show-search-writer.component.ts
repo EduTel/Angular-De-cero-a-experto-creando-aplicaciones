ec86657b4f0d29338c7e9b6af39e8ab1bb1d6682
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-07-16 11:34:40 -0500

    extends

--- 02SinglePageAplication/src/app/shared/show-search-writer/show-search-writer.component.ts
+++ 02SinglePageAplication/src/app/shared/show-search-writer/show-search-writer.component.ts
@@ -4,15 +4,18 @@ import { ActivatedRoute } from '@angular/router';
 import { WriterService, Writer } from '../../servicios/writes.service';
 import { WritersComponent } from '../writers/writers.component';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-show-search-writer',
   templateUrl: './show-search-writer.component.html',
   // styleUrls: ['./show-search-writer.component.css']
 })
-export class ShowSearchWriterComponent implements OnInit  {
+export class ShowSearchWriterComponent extends WritersComponent implements OnInit  {
   private writes: Writer[];
   private termino: string;
-  constructor(private _ActivatedRoute: ActivatedRoute, private _write_service: WriterService) {
+  constructor(private _ActivatedRoute: ActivatedRoute, private _write_service: WriterService, private _router: Router) {
+    super(_write_service, _router);
     this._ActivatedRoute.params.subscribe(parametros => {
       console.log(parametros['termino']);
       this.termino = parametros['termino'];
