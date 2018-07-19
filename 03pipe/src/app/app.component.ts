b56015b3a853918c57e24499c95a788b86d32517
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-07-19 15:32:00 -0500

    pipe json

--- 03pipe/src/app/app.component.ts
+++ 03pipe/src/app/app.component.ts
@@ -10,4 +10,18 @@ export class AppComponent {
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   e = Math.PI ;
+
+  object = {
+    Libro: 'el hombre en busca de sentido',
+    Hojas: '190',
+    Autor: {
+      Nombre: 'victor E. Frankl',
+      Edad: '92',
+      Nacimiento: '26 de marzo de 1905',
+      Fallecimiento: '2 de septiembre de 1997',
+      conocido_por: [ 'logoterapia', 'psicología', 'existencial']
+    }
+  };
+  cast_JSON: string = JSON.stringify(this.object);
+
 }
