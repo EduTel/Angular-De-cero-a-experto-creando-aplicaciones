f3f10c91459cff2517594e5365206d0e00ca422c
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-08-09 12:54:58 -0500

    Lifecycle Hooks (OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked y OnDestroy)

--- 06supermarket/src/app/components/home/home.component.ts
+++ 06supermarket/src/app/components/home/home.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  constructor() {
+    console.log('HomeComponent constructor');
   }
-
+  ngOnInit() {
+    console.log('HomeComponent ngOnInit');
+  }
+  ngOnChanges() {
+    console.log('HomeComponent ngOnChanges');
+  }
+  ngDoCheck() {
+    console.log('HomeComponent ngDoCheck');
+  }
+  ngAfterContentInit() {
+    console.log('HomeComponent ngAfterContentInit');
+  }
+  ngAfterContentChecked() {
+    console.log('HomeComponent ngAfterContentChecked');
+  }
+  ngAfterViewInit() {
+    console.log('HomeComponent ngAfterViewInit');
+  }
+  ngAfterViewChecked() {
+    console.log('HomeComponent ngAfterViewChecked');
+  }
+  ngOnDestroy() {
+    console.log('HomeComponent ngOnDestroy');
+  }
 }
