2763e0261381a7034e4475a52a65c818fbab4ace
EduardoTellez <15623704+EduTel@users.noreply.github.com>
2018-08-13 17:47:23 -0500

    asignar referencia #, type ngModel y errors

--- 08form/src/app/components/template/template.component.ts
+++ 08form/src/app/components/template/template.component.ts
@@ -4,7 +4,20 @@ import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    input.ng-touched.ng-invalid {
+      border: 1px solid red;
+    }
+    form.ng-touched.ng-valid {
+      border: 1px solid green;
+    }
+    input.ng-touched.ng-valid {
+      border: 1px solid green;
+    }
+    .msg_error{
+      color: red;
+    }
+  `]
 })
 export class TemplateComponent implements OnInit {
   c_usuario: Object = {
@@ -12,16 +25,22 @@ export class TemplateComponent implements OnInit {
     apellido: 'Mi apellido',
     mail: 'mimail@outlook.com'
   };
+  mi_form: NgForm;
   constructor() { }
 
   ngOnInit() {
   }
   onSubmit(f: NgForm) {
+    this.mi_form = f;
     console.log('my form');
     console.log(f);
+    console.log('*****************controls:');
+    console.log(f.controls);  // { first: '', last: '' }
+    console.log('*****************value:');
     console.log(f.value);  // { first: '', last: '' }
+    console.log('*****************valid:');
     console.log(f.valid);  // false
-    console.log('c_usuario:');
+    console.log('*****************c_usuario:');
     console.log(this.c_usuario);
   }
 
