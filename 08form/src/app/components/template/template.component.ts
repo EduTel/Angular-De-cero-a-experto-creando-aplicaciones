a4bbe3cb7dcaf0aeb6e61569cfbaf1c0abf8a01d
EduardoTellez <15623704+EduTel@users.noreply.github.com>
2018-08-13 17:06:32 -0500

    ngModel [ y ngModel (

--- 08form/src/app/components/template/template.component.ts
+++ 08form/src/app/components/template/template.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class TemplateComponent implements OnInit {
-
+  c_usuario: Object = {
+    nombre: 'Mi nombre',
+    apellido: 'Mi apellido',
+    mail: 'mimail@outlook.com'
+  };
   constructor() { }
 
   ngOnInit() {
   }
-  onSubmit() {
+  onSubmit(f: NgForm) {
     console.log('my form');
+    console.log(f);
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+    console.log('c_usuario:');
+    console.log(this.c_usuario);
   }
 
 }
