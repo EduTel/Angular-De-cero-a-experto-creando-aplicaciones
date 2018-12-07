0e1b71e9461269a1863a1eb84e6a0bf4a1e18f78
EduTel <15623704+EduTel@users.noreply.github.com>
2018-12-06 18:50:38 -0600

    cambios

--- 09HeroesMyFirebase/src/app/app.module.ts
+++ 09HeroesMyFirebase/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-import { HeroeComponent } from './components/heroes/heroe/heroe.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
+import { appRouting } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroeComponent,
     HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    appRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
